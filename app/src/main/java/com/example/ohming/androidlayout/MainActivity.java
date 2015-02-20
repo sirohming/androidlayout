5ddf3b9e5976cdde1ce1428e719aaa9edd701038
Ohming <sirohming@gmail.com>
2015-02-20 12:10:27 +0700

    1

---
+++ app/src/main/java/com/example/ohming/androidlayout/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ohming.androidlayout;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TableLayout test = (TableLayout)findViewById(R.id.table);
+        String[] data = new String[]{"Ohmimg","Jozzee","Pad"};
+        for(String str:data){
+            TableRow tRow = new TableRow(this);
+            TextView txt = new TextView(this);
+            txt.setText(str);
+            tRow.addView(txt);
+            test.addView(tRow);
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
